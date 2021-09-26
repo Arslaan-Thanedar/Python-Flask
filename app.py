2f936baeaf1fcbfe20beffc3190e47fc151fde86
Arslaan <91215459+Arslaan-Thanedar@users.noreply.github.com>
2021-09-26 12:01:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+    return ''' ' Learned basics to devlope a website '\n\n
+
+
+                   \n\nWhatever you do, do it well. 
+                 
+                                               – Walt Disney   ''' 
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
